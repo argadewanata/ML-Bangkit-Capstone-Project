c078dca25c4a441a3e38d1f271ce6ff424a87da9
Arga Dewanata <rerearga@gmail.com>
2023-05-21 16:09:28 +0700

    feat: Support get data from one place

---
+++ GetData/GetJSONfromOnePlace.py
@@ -0,0 +1,55 @@
+# Import libraries
+import requests
+import json
+
+# Define constants
+REGION = "id"
+LANGUAGE = "id"
+API_KEY = "AIzaSyAnZNq3mo7LON6UyvVbvnqum-wzxtV31Nk"
+PLACE_ID = "ChIJvb0GwNhYei4R1QHPGGdxI_A"  # Filosopi Kopi Jogja
+PLACE_URL = "https://maps.googleapis.com/maps/api/place/details/json"
+
+# Define categories
+basic_category = ["address_components", "adr_address", "business_status", "formatted_address",
+                  "geometry", "icon", "icon_mask_base_uri", "icon_background_color",
+                  "name", "photo", "place_id", "plus_code", "type", "url", "utc_offset", "vicinity",
+                  "wheelchair_accessible_entrance"]
+
+contact_category = ["formatted_phone_number", "international_phone_number",
+                    "opening_hours", "secondary_opening_hours", "website"]
+
+atmosphere_category = ["curbside_pickup", "delivery", "dine_in", "editorial_summary",
+                       "price_level", "rating", "reservable", "reviews", "serves_beer",
+                       "serves_breakfast", "serves_brunch", "serves_dinner", "serves_lunch",
+                       "serves_vegetarian_food", "serves_wine", "takeout", "user_ratings_total"]
+
+show2android_fields = ["name", "rating", "opening_hours", "price_level",
+                       "user_ratings_total", "reviews", "types", "website",
+                       "formatted_phone_number", "formatted_address", "geometry",
+                       "serves_wine", "serves_beer", "wheelchair_accessible_entrance"]
+
+all_possible_fields = basic_category + contact_category + atmosphere_category
+
+# Define parameters
+params = {
+    "place_id": PLACE_ID,
+    # "fields": ','.join(all_possible_fields),
+    "fields": ','.join(show2android_fields),
+    "key": API_KEY,
+    "language": LANGUAGE,
+    "region": REGION,
+    # "opennow": True,
+    "reviews_no_translations": True,
+}
+
+# GET data from Google Places API
+response = requests.get(PLACE_URL, params=params)
+
+# Convert data to JSON
+data = response.json()
+
+# Save data to JSON file
+with open("./response/1place_selected_fields.json", "w") as file:
+    json.dump(data, file)
+
+print("DONE")
