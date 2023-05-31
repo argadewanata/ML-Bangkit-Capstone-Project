56113ef97a85c9dbc3695fad0c3a6e84810555b0
Arga Dewanata <rerearga@gmail.com>
2023-06-01 03:31:45 +0700

    Feat : Add support SQL Insert statement

--- Database/CSVtoMySQL.py
+++ Database/CSVtoMySQL.py
@@ -1,24 +1,38 @@
 import csv
 import os
 
-DIR_PATH = "./Database/CSV/"
-CSV_FILENAME = "Places.csv"
-CSV_PATH = os.path.join(DIR_PATH, CSV_FILENAME)
-
-print(CSV_PATH)
-
+CSV_FILEPATH = "Database/CSV/Places.csv"
+SQL_FILEPATH = "Database/PlacesInsertData.sql"
 
 def generate_insert_statement(row):
     insert_statement = "INSERT INTO Places VALUES ("
-    insert_statement += "'{}', '{}', '{}', '{}', {}, {}, {}, {}, {}, {}, '{}', {}, {}, '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(
-        row['Places_ID'], row['Name'], row['FormattedAddress'], row['FormattedPhone'], row['Latitude'], row['Longitude'], row['rating'], row['ServesBeer'], row['ServesWine'], row['UserRatingTotal'], row[
-            'Website'], row['PriceLevel'], row['WheelchairAccessibleEntrance'], row['StreetAddress'], row['District'], row['City'], row['Regency'], row['Province'], row['PostalNumber'], row['PhotoLink']
+    insert_statement += "'{}', '{}', '{}', '{}', {}, {}, {}, {}, '{}', {}, {}, {}, {}, {}, {}, '{}', '{}', '{}', '{}', '{}');".format(
+        row['Place_ID'], row['Name'], row['FormattedPhone'], row['FormattedAddress'], row['Latitude'], 
+        row['Longitude'], row['OverallRating'],row['PriceLevel'], row['Website'], row['UserRatingTotal'], 
+        row['ServesBeer'], row['ServesWine'], row['ServesVegetarianFood'], row['WheelchairAccessibleEntrance'], row['Halal'], 
+        row['StreetAddress'], row['District'], row['City'], row['Regency'],row['Province'],row['PostalNumber']  
     )
     return insert_statement
 
-
-with open(CSV_PATH, 'r') as csvfile:
+with open(CSV_FILEPATH, 'r', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        insert_statement = generate_insert_statement(row)
-        print(insert_statement)
+    total_rows = sum(1 for row in reader)
+    csvfile.seek(0)  # Reset the file pointer
+    next(reader)  # Skip the header row
+    with open(SQL_FILEPATH, 'w', encoding='utf-8') as sqlfile:
+        for i, row in enumerate(reader, 1):
+            insert_statement = generate_insert_statement(row)
+            sqlfile.write(insert_statement)
+            if i != total_rows:
+                sqlfile.write("\n")  # Add a new line after each insert statement, except for the last one
+            print("Progress: {}/{}".format(i, total_rows))
+
+print("DONE!")  
+
+
+
+
+
+
+
+
--- Database/CreateTable.sql
+++ Database/CreateTable.sql
@@ -1,51 +1,51 @@
 -- Create Table SQL Script 
 
-CREATE TABLE Places(
-    Places_ID VARCHAR(35) PRIMARY KEY,
-    Name VARCHAR(40) NOT NULL,
-    FormattedPhone VARCHAR(15) NOT NULL,
+CREATE TABLE Places (
+    Place_ID VARCHAR(45) PRIMARY KEY,
+    Nama VARCHAR(40) NOT NULL,
+    FormattedPhone VARCHAR(25) NOT NULL,
     FormattedAddress VARCHAR(70) NOT NULL,
     Latitude FLOAT NOT NULL,
     Longitude FLOAT NOT NULL,
-    OverallRating FLOAT(2, 2) NOT NULL,
-    PriceLevel INT NOT NULL,
-    Website VARCHAR(50) NOT NULL,
-    UserRatingTotal INT NOT NULL,
-    ServesBeer BOOLEAN NOT NULL,
-    ServesWine BOOLEAN NOT NULL,
-    ServesVegetarian BOOLEAN NOT NULL,
-    Halal BOOLEAN NOT NULL,
-    WheelchairAccesibleEntrance BOOLEAN NOT NULL,
+    OverallRating FLOAT,
+    PriceLevel FLOAT,
+    Website VARCHAR(50),
+    UserRatingTotal FLOAT,
+    ServesBeer BOOLEAN,
+    ServesWine BOOLEAN,
+    ServesVegetarian BOOLEAN,
+    WheelchairAccessibleEntrance BOOLEAN,
+    Halal BOOLEAN,
     StreetAddress VARCHAR(40),
-    District VARCHAR(20),
-    City VARCHAR(20),
-    Regency VARCHAR(20),
-    Province VARCHAR(20),
-    PostalNumber VARCHAR(10),
-    PhotoLink VARCHAR(50)
+    District VARCHAR(25),
+    City VARCHAR(25),
+    Regency VARCHAR(25),
+    Province VARCHAR(25),
+    PostalNumber VARCHAR(25)
 );
 
+
 CREATE TABLE Categories(
-    Places_ID VARCHAR(35),
+    Place_ID VARCHAR(35),
     Cafe BOOLEAN,
     Restaurant BOOLEAN,
     Bar BOOLEAN,
-    FOREIGN KEY(Places_ID) REFERENCES Places(Places_ID)
+    FOREIGN KEY(Place_ID) REFERENCES Places(Place_ID)
 );
 
 CREATE TABLE Reviews(
-    Places_ID VARCHAR(35),
+    Place_ID VARCHAR(35),
     Name VARCHAR(25),
     Time VARCHAR(30),
     Experience VARCHAR(15),
     NumberOfReview INT,
     ReviewText VARCHAR(1000),
     Star INT,
-    FOREIGN KEY(Places_ID) REFERENCES Places(Places_ID)
+    FOREIGN KEY(Place_ID) REFERENCES Places(Place_ID)
 );
 
 CREATE TABLE WorkingHour(
-    Places_ID VARCHAR(35),
+    Place_ID VARCHAR(35),
     MondayOpenHour CHAR(5),
     MondayCloseHour CHAR(5),
     TuesdayOpenHour CHAR(5),
@@ -60,6 +60,6 @@ CREATE TABLE WorkingHour(
     SaturdayCloseHour CHAR(5),
     SundayOpenHour CHAR(5),
     SundayCloseHour CHAR(5),
-    FOREIGN KEY(Places_ID) REFERENCES Places(Places_ID)
+    FOREIGN KEY(Place_ID) REFERENCES Places(Place_ID)
 );
 
