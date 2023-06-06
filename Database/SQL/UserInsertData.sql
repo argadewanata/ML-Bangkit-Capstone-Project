61827248139a348f880d10c96a139a6f80941103
Arga Dewanata <rerearga@gmail.com>
2023-06-06 22:57:15 +0700

    feat: add user insert SQL

---
+++ Database/SQL/UserInsertData.sql
@@ -0,0 +1,5 @@
+INSERT INTO User(User_ID,User_Name,Email,Password) VALUES ('001','Andi Budi', 'andibudi@gmail.com', 'andibudi123');
+INSERT INTO User(User_ID,User_Name,Email,Password) VALUES ('002','Caca Dina', 'cacadina@gmail.com', 'cacadina123');
+INSERT INTO User(User_ID,User_Name,Email,Password) VALUES ('003','Esty Farah', 'estyfarah@gmail.com', 'estyfarah123');
+INSERT INTO User(User_ID,User_Name,Email,Password) VALUES ('004','Galih Handoko', 'galihhandoko@gmail.com', 'galihhandoko123');
+INSERT INTO User(User_ID,User_Name,Email,Password) VALUES ('005','Insan Joko', 'insanjoko@gmail.com', 'insanjoko123');
