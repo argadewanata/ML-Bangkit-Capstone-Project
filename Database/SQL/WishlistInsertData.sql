e9adcd5bee95aac32b1b3fc09f8e93460d630f26
Arga Dewanata <rerearga@gmail.com>
2023-06-11 20:12:12 +0700

    fix: Insert Wishlist syntax error

--- Database/SQL/WishlistInsertData.sql
+++ Database/SQL/WishlistInsertData.sql
@@ -1,35 +1,35 @@
 -- Andi Budi 
-INSERT INTO User(User_ID,Place_ID) VALUES ('001','ChIJr8VgoBYBey4RPgC9oYEES0M');
-INSERT INTO User(User_ID,Place_ID) VALUES ('001','ChIJs-e74Ihbei4R0xYwqyguij0');
-INSERT INTO User(User_ID,Place_ID) VALUES ('001','ChIJ_WKkLNxXei4R7Jh9_wrcp9U');
-INSERT INTO User(User_ID,Place_ID) VALUES ('001','ChIJyff7yXVXei4RgIxBaCM9e2c');
-INSERT INTO User(User_ID,Place_ID) VALUES ('001','ChIJ12iZq5ZXei4RgQuh1QR2C3c');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('001','ChIJr8VgoBYBey4RPgC9oYEES0M');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('001','ChIJs-e74Ihbei4R0xYwqyguij0');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('001','ChIJ_WKkLNxXei4R7Jh9_wrcp9U');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('001','ChIJyff7yXVXei4RgIxBaCM9e2c');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('001','ChIJ12iZq5ZXei4RgQuh1QR2C3c');
 
 -- Caca Dina
-INSERT INTO User(User_ID,Place_ID) VALUES ('002','ChIJJdrijK8Bey4RLJd-873sz5w');
-INSERT INTO User(User_ID,Place_ID) VALUES ('002','ChIJl6tDx8dZei4RfA88ffLXnDc');
-INSERT INTO User(User_ID,Place_ID) VALUES ('002','ChIJx9si9MVZei4R7k80s8E6a3Q');
-INSERT INTO User(User_ID,Place_ID) VALUES ('002','ChIJm-Y01qpZei4RBbzEQdCI1js');
-INSERT INTO User(User_ID,Place_ID) VALUES ('002','ChIJzXaqSjJYei4RYf7_5RITA9Y');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('002','ChIJJdrijK8Bey4RLJd-873sz5w');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('002','ChIJl6tDx8dZei4RfA88ffLXnDc');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('002','ChIJx9si9MVZei4R7k80s8E6a3Q');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('002','ChIJm-Y01qpZei4RBbzEQdCI1js');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('002','ChIJzXaqSjJYei4RYf7_5RITA9Y');
 
 -- Esty Farah
-INSERT INTO User(User_ID,Place_ID) VALUES ('003','ChIJlUDN8zBYei4RQ1NeDlgcJu0');
-INSERT INTO User(User_ID,Place_ID) VALUES ('003','ChIJ51nfgEFZei4RJwcTMc5VXW4');
-INSERT INTO User(User_ID,Place_ID) VALUES ('003','ChIJbc2bgURYei4RAw8upAxTexE');
-INSERT INTO User(User_ID,Place_ID) VALUES ('003','ChIJHytZNZRZei4RRbqBOVA3qW8');
-INSERT INTO User(User_ID,Place_ID) VALUES ('003','ChIJd7M66J1Zei4RVqoL_YKHkeg');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('003','ChIJlUDN8zBYei4RQ1NeDlgcJu0');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('003','ChIJ51nfgEFZei4RJwcTMc5VXW4');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('003','ChIJbc2bgURYei4RAw8upAxTexE');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('003','ChIJHytZNZRZei4RRbqBOVA3qW8');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('003','ChIJd7M66J1Zei4RVqoL_YKHkeg');
 
 -- Galih Handoko
-INSERT INTO User(User_ID,Place_ID) VALUES ('004','ChIJz8NNJzpYei4R9Wh7rwSq9cY');
-INSERT INTO User(User_ID,Place_ID) VALUES ('004','ChIJu4La5qJXei4Rv_tS1ldTqms');
-INSERT INTO User(User_ID,Place_ID) VALUES ('004','ChIJjf3V6rxXei4R0lNHoFphUss');
-INSERT INTO User(User_ID,Place_ID) VALUES ('004','ChIJu-N07Fdnei4RjraVCHptiAY');
-INSERT INTO User(User_ID,Place_ID) VALUES ('004','ChIJ2RN9_2Bnei4RdD0ODTbeVY8');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('004','ChIJz8NNJzpYei4R9Wh7rwSq9cY');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('004','ChIJu4La5qJXei4Rv_tS1ldTqms');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('004','ChIJjf3V6rxXei4R0lNHoFphUss');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('004','ChIJu-N07Fdnei4RjraVCHptiAY');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('004','ChIJ2RN9_2Bnei4RdD0ODTbeVY8');
 
 -- Galih Handoko
-INSERT INTO User(User_ID,Place_ID) VALUES ('005','ChIJLxsiXtJZei4RLxJtpnDu3Wc');
-INSERT INTO User(User_ID,Place_ID) VALUES ('005','ChIJT2G5C-4Bey4RF229hMb8nMI');
-INSERT INTO User(User_ID,Place_ID) VALUES ('005','ChIJf12tTJ4Aey4RVWk5JDdChEk');
-INSERT INTO User(User_ID,Place_ID) VALUES ('005','ChIJhxlbB2sBey4Rd_mrw7i6OaU');
-INSERT INTO User(User_ID,Place_ID) VALUES ('005','ChIJE5n1K58Aey4RLQwDvRIuWiQ');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('005','ChIJLxsiXtJZei4RLxJtpnDu3Wc');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('005','ChIJT2G5C-4Bey4RF229hMb8nMI');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('005','ChIJf12tTJ4Aey4RVWk5JDdChEk');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('005','ChIJhxlbB2sBey4Rd_mrw7i6OaU');
+INSERT INTO Wishlist(User_ID,Place_ID) VALUES ('005','ChIJE5n1K58Aey4RLQwDvRIuWiQ');
 
